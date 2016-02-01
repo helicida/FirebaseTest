f95159d4cef2084804029f14389ab2c45a876a68
Sergi Barjola <sergibca@gmail.com>
2016-02-01 18:40:17 +0100

    Mostramos una lista con datos extraídos de Firebase

--- app/src/main/java/helicida/firebasetest/MainActivityFragment.java
+++ app/src/main/java/helicida/firebasetest/MainActivityFragment.java
@@ -2,22 +2,29 @@ package helicida.firebasetest;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.ui.FirebaseListAdapter;
+
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    private Firebase ref;
+    Firebase ref;
+    Firebase users;
+    TextView text1, text2;
 
     public MainActivityFragment() {}
 
@@ -30,9 +37,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+        final View fragmentoLista = inflater.inflate(R.layout.fragment_main, container, false); //Definimos el fragment
+
         configFirebase();
 
         ref = new Firebase("https://helicidatest.firebaseio.com/");
+
+        users = ref.child("users");
+
+        users.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e("XXXX", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        /*
 
         ref.child("message").setValue("Mensaje prueba");
 
@@ -46,10 +71,10 @@ public class MainActivityFragment extends Fragment {
         mireiaRef.setValue(mireia);
 
         // Usando push automaitcamente genera la clave de referencia
-        /*
+
         Firebase users = ref.child("users");
         Firebase usersRef = users.push();
-        usersRef.setValue(new User("Jordi", 2001));*/
+        usersRef.setValue(new User("Jordi", 2001));
 
         // Ejemplo de como leer información de la BBDD
         ref.child("users").child("sergi").addValueEventListener(new ValueEventListener() {
@@ -63,9 +88,25 @@ public class MainActivityFragment extends Fragment {
             public void onCancelled(FirebaseError firebaseError) {
 
             }
-        });
+        });*/
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listUsers = (ListView) fragmentoLista.findViewById(R.id.listView1);
+
+        FirebaseListAdapter mAdapter = new FirebaseListAdapter<User>(getActivity(), User.class, R.layout.adapter_firebase_users_layout, users) {
+                @Override
+                protected void populateView(View view, User user, int position) {
+                    super.populateView(view, user, position);
+                    text1 = (TextView) view.findViewById(R.id.text1);
+                    text1.setText(user.getFullName());
+
+                    text2 = (TextView) view.findViewById(R.id.text2);
+                    text2.setText(String.valueOf(user.getBirthYear()));
+                }
+        };
+
+        listUsers.setAdapter(mAdapter);
+
+        return fragmentoLista;
     }
 
     public void configFirebase(){
--- app/src/main/java/helicida/firebasetest/User.java
+++ app/src/main/java/helicida/firebasetest/User.java
@@ -6,17 +6,20 @@ import com.firebase.client.Firebase;
  * Created by 46465442z on 27/01/16.
  */
 public class User {
+
     private int birthYear;
     private String fullName;
 
-    public User() {}
+    public User() {
+
+    }
 
     public User(String fullName, int birthYear) {
         this.fullName = fullName;
         this.birthYear = birthYear;
     }
 
-    public long getBirthYear() {
+    public int getBirthYear() {
         return birthYear;
     }
 
